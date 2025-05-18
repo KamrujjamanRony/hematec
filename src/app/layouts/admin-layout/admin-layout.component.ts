77cf35d682a39064cce89adca493e6a06f52bf67
KamrujjamanRony <kamrujjaman0228@gmail.com>
2025-05-18 18:45:11 +0600

    json data added

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '@environments/environments';
 import { SidebarComponent } from 'app/components/sidebar/sidebar.component';
+import { DataService } from 'app/features/services/data.service';
 
 @Component({
-    selector: 'app-admin-layout',
-    templateUrl: './admin-layout.component.html',
-    styleUrls: ['./admin-layout.component.css'],
-    imports: [RouterOutlet, FormsModule, SidebarComponent]
+  selector: 'app-admin-layout',
+  templateUrl: './admin-layout.component.html',
+  styleUrls: ['./admin-layout.component.css'],
+  imports: [RouterOutlet, FormsModule, SidebarComponent]
 })
 export class AdminLayoutComponent {
+  private readonly dataService = inject(DataService);
+  password: string = "";
   isAuthorized: boolean = false;
   pass: string = "";
   err: string = '';
 
+  ngOnInit() {
+    // Fetch the password from the JSON file
+    this.dataService.getPassword().subscribe((data: any) => {
+      this.password = data;
+    });
+  }
+
   onSubmitAuth(data: any): void {
-    this.pass === environment.authKey ? this.isAuthorized = true : this.err = "Please enter correct password";
+    this.pass === this.password ? this.isAuthorized = true : this.err = "Please enter correct password";
   }
 }
